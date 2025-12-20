96478d02d34afa972aa9559fe6ceac04f09f5669
Guimeng Liu <57077731+Guimeng-Leo-Liu@users.noreply.github.com>
2025-12-20 12:07:10 +0800

    Add files via upload
    Signed-off-by: Guimeng Liu <57077731+Guimeng-Leo-Liu@users.noreply.github.com>

---
+++ static/js/gmdc-sankey-data.js
@@ -0,0 +1,416 @@
+window.GMDC_SANKEY_DATA = [
+  {
+    "method": "TGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "TGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "cGM-3"
+  },
+  {
+    "method": "BSA",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "FreezeD",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "MineGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "uGM-2"
+  },
+  {
+    "method": "MineGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "cGM-3"
+  },
+  {
+    "method": "MineGAN++",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "uGM-2"
+  },
+  {
+    "method": "MineGAN++",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "cGM-3"
+  },
+  {
+    "method": "AdaFMGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "uGM-2"
+  },
+  {
+    "method": "EWC",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "GAN-Memory",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "uGM-2"
+  },
+  {
+    "method": "CDC",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "DCL",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "RSSA",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "AdAM",
+    "approach": "Transfer Learning",
+    "subcategory": "Adaptation-Aware",
+    "task": "uGM-2"
+  },
+  {
+    "method": "AdAM",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "uGM-2"
+  },
+  {
+    "method": "RICK",
+    "approach": "Transfer Learning",
+    "subcategory": "Adaptation-Aware",
+    "task": "uGM-2"
+  },
+  {
+    "method": "StyleGAN-NADA",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-3"
+  },
+  {
+    "method": "MTG",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "HyperDomainNet",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "HyperDomainNet",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "uGM-3"
+  },
+  {
+    "method": "GenOS",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "DynaGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "uGM-2"
+  },
+  {
+    "method": "GenDA",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "uGM-2"
+  },
+  {
+    "method": "SVD",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "JoJoGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "OneCLIP",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "IPL",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-3"
+  },
+  {
+    "method": "VPT",
+    "approach": "Transfer Learning",
+    "subcategory": "Prompt Tuning",
+    "task": "cGM-2"
+  },
+  {
+    "method": "VPT",
+    "approach": "Transfer Learning",
+    "subcategory": "Prompt Tuning",
+    "task": "cGM-3"
+  },
+  {
+    "method": "SINE",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "DreamBooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Custom Diffusion",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "MaskD",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "ICGAN",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "cGM-2"
+  },
+  {
+    "method": "MultiDiffusion",
+    "approach": "Transfer Learning",
+    "subcategory": "Latent Space",
+    "task": "uGM-2"
+  },
+  {
+    "method": "Textual-Inversion",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "ELITE",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "E4T",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "AblateConcept",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "Domain Expansion",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-3"
+  },
+  {
+    "method": "ProSpect",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Instantbooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Hyperdreambooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Hyperdreambooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "MaskedD",
+    "approach": "Transfer Learning",
+    "subcategory": "Masking",
+    "task": "uGM-2"
+  },
+  {
+    "method": "AnomalyDiffusion",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "FairQueue",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-2"
+  },
+  {
+    "method": "FairTL",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "uGM-2"
+  },
+  {
+    "method": "OMG",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "TFIC",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "HyperDreamBooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "AnyDoor",
+    "approach": "Transfer Learning",
+    "subcategory": "Regularizer-based Fine-Tuning",
+    "task": "SGM"
+  },
+  {
+    "method": "FastComposer",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "ZipLoRA",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Orthogonal Adaptation",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Orthogonal Adaptation",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "Mix-of-Show",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "Mix-of-Show",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "DreamMatcher",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "DreamMatcher",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "SSR-Encoder",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "PortraitBooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "PortraitBooth",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "IDAdapter",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "L2DM",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "MagiCapture",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "SGM"
+  },
+  {
+    "method": "MagiCapture",
+    "approach": "Transfer Learning",
+    "subcategory": "Modulation",
+    "task": "SGM"
+  },
+  {
+    "method": "AIR",
+    "approach": "Transfer Learning",
+    "subcategory": "Natural Language-Guided",
+    "task": "uGM-3"
+  }
+];// Generated from data_tmlr_final.csv
